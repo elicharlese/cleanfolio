0bbb0ea12231e045d75724ca2b2b2ebc45f9a076
elicharlese <elicharles.e@gmail.com>
2022-06-08 22:15:51 -0700

    JS to build dashboard and wallet

---
+++ src/pages/authentication/sign-in.js
@@ -0,0 +1,25 @@
+// < !DOCTYPE html >
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <link rel="stylesheet" href="\public\css\main.css">
+// </head>
+// <body>
+//     <nav>
+//         <div class="sidebar">
+//             <a href="/public/index.html">
+//                 <img class="logo" src="/images/CEC-logo.png" alt="Cleanfolio Logo" />
+//             </a>
+//         </div>
+//         <ul class="nav-bar">
+//             <li class="nav-button"><a href="pages/hire-me.html">Hire Me</a></li>
+//             <li class="nav-button"><a href="https://github.com/elicharlese">GitHub</a></li>
+//             <li class="nav-button"><a href="#open-source">Open Source</a></li>
+//             <li class="nav-button"><a href="pages/organizations.html">Organizations</a></li>
+//             <div class="button"><a href="pages/login.html">Login</div>
+//         </ul>
+//     </nav>
+// <body>
+// </html>
